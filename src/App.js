a44c530ecbe2ab7b3fff367c9e4d34889c935ada
Eduardo Samuel <133702228+1eduardosm@users.noreply.github.com>
2025-08-10 23:20:16 -0300

    teste

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Header">
+        <p>teste</p>
+      </div>
+      <div className="Body">
+        <p>teste</p>
+      </div>
+      <div className="Footer">
+        <p>teste</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
