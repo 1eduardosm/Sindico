6f15337cf7aa4ce3f47e643fbb9f7fd3705458a9
Eduardo Samuel <133702228+1eduardosm@users.noreply.github.com>
2025-08-18 00:23:36 -0300

    Adicionando algumas navegações e estrutura base para as seções

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import './App.css';
+import { useRef } from "react";
+
 
 function App() {
+  const homeRef = useRef(null);
+  const vantagensRef = useRef(null);
+  const contatoRef = useRef(null);
+
+
+  const scrollToRef = (ref) => {
+    if (ref && ref.current) {
+      ref.current.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
   return (
     <div className="App">
       <div className="Header">
-        <p>Site funcionando!</p>
+        <div className='Hsw'>
+          <p>Sindico Profissional</p>
+        </div>
+        <div className='Navigation'>
+          <p onClick={() => scrollToRef(homeRef)}>Home</p>
+          <p onClick={() => scrollToRef(vantagensRef)}>Vantagens</p>
+          <p onClick={() => scrollToRef(contatoRef)}>Contato</p>
+        </div>
+        <div className="Logo">
+          <img src="/Logo.png" alt="Logo" />
+        </div>
       </div>
       <div className="Body">
-        <p>Vou começar a fazer de noite</p>
+        <div className="Home" ref={homeRef}>
+
+        </div>
+        <div className="Vantagens" ref={vantagensRef}>
+        
+        </div>
+        <div className="Contato" ref={contatoRef}>
+        
+        </div>
       </div>
       <div className="Footer">
         <p>Eu te amo pai!</p>
       </div>
-    </div>
+    </div >
   );
 }
 
