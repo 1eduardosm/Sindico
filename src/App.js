31647d6f946b4384da4568d593310552be0d9ca3
Eduardo Samuel <133702228+1eduardosm@users.noreply.github.com>
2025-08-18 02:29:44 -0300

    tinha esquecido de redimensinar para celular e etc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
+import React, { useRef } from "react";
 import './App.css';
-import { useRef } from "react";
-
 
 function App() {
   const homeRef = useRef(null);
   const vantagensRef = useRef(null);
   const contatoRef = useRef(null);
-
 
   const scrollToRef = (ref) => {
     if (ref && ref.current) {
@@ -17,10 +15,10 @@ function App() {
   return (
     <div className="App">
       <div className="Header">
-        <div className='Hsw'>
+        <div className="Hsw">
           <p>Sindico Profissional</p>
         </div>
-        <div className='Navigation'>
+        <div className="Navigation">
           <p onClick={() => scrollToRef(homeRef)}>Home</p>
           <p onClick={() => scrollToRef(vantagensRef)}>Vantagens</p>
           <p onClick={() => scrollToRef(contatoRef)}>Contato</p>
@@ -29,21 +27,18 @@ function App() {
           <img src="/Logo.png" alt="Logo" />
         </div>
       </div>
-      <div className="Body">
-        <div className="Home" ref={homeRef}>
 
-        </div>
-        <div className="Vantagens" ref={vantagensRef}>
-        
-        </div>
-        <div className="Contato" ref={contatoRef}>
-        
-        </div>
+      <div className="Body">
+        <div className="Home" ref={homeRef}>Home</div>
+        <div className="Vantagens" ref={vantagensRef}>Vantagens</div>
+        <div className="Contato" ref={contatoRef}>Contato</div>
       </div>
+
+      {/* Footer */}
       <div className="Footer">
         <p>Eu te amo pai!</p>
       </div>
-    </div >
+    </div>
   );
 }
 
