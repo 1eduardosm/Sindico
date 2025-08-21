28228bd9630900f549ad22405b00f8ecef5711f3
Eduardo Samuel <133702228+1eduardosm@users.noreply.github.com>
2025-08-21 20:34:45 -0300

    header finalizada celular sem div redes pc completo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import './App.css';
 
 function App() {
   const homeRef = useRef(null);
   const vantagensRef = useRef(null);
   const contatoRef = useRef(null);
+
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1024);
+
+  useEffect(() => {
+    const handleResize = () => setIsDesktop(window.innerWidth >= 1024);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const scrollToRef = (ref) => {
     if (ref && ref.current) {
@@ -19,15 +27,43 @@ function App() {
           <img src="/Logo.png" alt="Logo" />
         </div>
         <div className="Navigation">
-          <p onClick={() => scrollToRef(homeRef)}>Home</p>
+          <p onClick={() => scrollToRef(homeRef)}>Início</p>
           <p onClick={() => scrollToRef(vantagensRef)}>Vantagens</p>
           <p onClick={() => scrollToRef(contatoRef)}>Contato</p>
         </div>
-        <div className="Redes">
-          <p onClick={() => scrollToRef(homeRef)}>Home</p>
-          <p onClick={() => scrollToRef(vantagensRef)}>Vantagens</p>
-          <p onClick={() => scrollToRef(contatoRef)}>Contato</p>
-        </div>
+        {isDesktop && (
+          <div className="Redes">
+            <img
+              src="/icon_whatsapp.png"
+              alt="whatsapp"
+              onClick={() => {
+                const numero = "5547999629798"; 
+                const mensagem = "Olá, vi seu site, gostaria de mais informações";
+                const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+                window.open(url, "_blank");
+              }}
+              style={{ cursor: "pointer" }}
+            />
+            <img
+              src="/icon_instagram.png"
+              alt="instagram"
+              onClick={() => window.open("https://www.instagram.com/sindicocarlosmoraes?igsh=MWQ3ajM2eXJmbDVqdQ%3D%3D&utm_source=qr", "_blank")}
+              style={{ cursor: "pointer" }}
+            />
+            <img
+              src="/icon_facebook.png"
+              alt="gmail"
+              onClick={() => window.open("https://www.facebook.com/share/1BK6JfpUEo/?mibextid=wwXIfr", "_blank")}
+              style={{ cursor: "pointer" }}
+            />
+            <img
+              src="/icon_youtube.png"
+              alt="youtube"
+              onClick={() => window.open("https://www.youtube.com/", "_blank")}
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+        )}
       </div>
 
       <div className="Body">
