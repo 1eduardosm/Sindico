d37d0b917b0881d6165d13f9d0c7f016d82fa5c9
Pedro Venicio <pedrovenicio456@gmail.com>
2025-08-28 00:11:00 -0300

    validacao credencial (regra firebase)

--- src/Edit.js
+++ src/Edit.js
@@ -1,49 +1,69 @@
 import React, { use, useState } from 'react'
 import { getFirestore, database, doc, updateDoc, collection } from "./firebaseConfig";
 import { onSnapshot, query } from 'firebase/firestore';
-
-
+import { auth, onAuthStateChanged} from './firebaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 const Edit = () => {
 
   const [pesquisa, setPesquisa] = useState([])
   const [estado, setEstado] = useState('')
   const [input, setInput] = useState('')
+  const [login, setLogin] = useState('')
+  const [password, setPassword] = useState('')
 
   const name = collection(database, "Cliente")
 
-    onSnapshot(name, (query) => {
-      const list = []
-      query.forEach((doc) => {
-        const docData = doc.data();
-        list.push({ ...docData, id: doc.id })
+  onSnapshot(name, (query) => {
+    const list = []
+    query.forEach((doc) => {
+      const docData = doc.data();
+      list.push({ ...docData, id: doc.id })
 
-      })
-      setPesquisa(list)
     })
+    setPesquisa(list)
+  })
 
-    function edit(id) {
-      const clienteRef = doc(database, "Cliente", id)
-      updateDoc(clienteRef, {
-        cliente: input
-      })
-
+  function edit(id) {
+    const clienteRef = doc(database, "Cliente", id)
+    updateDoc(clienteRef, {
+      cliente: input
+    })
+  }
+  
+  async function logar() {
+    try {
+      await signInWithEmailAndPassword(auth, login, password)
     }
+    catch{
+      console.log('não foi possível logar')
+    }
+  }
 
   return (
     <div>
-      ------get------ (clica em um nome pra selecionar qual vai editar, digita no input e clica no botão que deve editar)
+      ------get------ 
       {pesquisa.map(item => (
         <div id={item.id} onClick={() => setEstado(item.id)}>
           {item.cliente}
         </div>
       ))}
-      <br></br>
       ------put------
       <div>
         <input placeholder='nome' onChange={(e) => setInput(e.target.value)} />
       </div>
       <button onClick={() => edit(estado)}>editar</button>
+      <div>
+        login
+        <div>
+          <input placeholder='login' onChange={(e) => setLogin(e.target.value)}/>
+        </div>
+        <div>
+          <input placeholder='senha' onChange={(e) => setPassword(e.target.value)}/>
+        </div>
+        <button onClick={() => logar()}>logar</button>
+
+      </div>
     </div>
   )
 }
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection, doc, deleteDoc, addDoc, updateDoc } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,6 +22,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+const auth = getAuth(app);
+
 const database = getFirestore(app)
 
-export { database, collection, doc, deleteDoc, addDoc, updateDoc }
+export { database, collection, doc, deleteDoc, addDoc, updateDoc, auth }
